88c380706587d1ad41cc65214239807a0db94e63
laviskis <v.sustick@gmail.com>
2024-11-05 10:35:48 +0100

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,7 +1,7 @@
 Office.onReady((info) => {
     if (info.host === Office.HostType.PowerPoint) {
         console.log("PowerPoint environment detected.");
-        document.getElementById("saveSlideButton").onclick = createNewPresentationWithSelectedSlides;
+        document.getElementById("createEmailButton").onclick = createNewPresentationWithSelectedSlides;
         initializeApp();
     }
 });
